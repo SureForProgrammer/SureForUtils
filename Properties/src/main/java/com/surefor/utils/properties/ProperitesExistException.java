e38acf1f8d7f023e40617778a0b428a1703f9c15
Ethan Cha <ethan.cha@pointclickcare.com>
2015-06-24 09:50:46 -0400

    Update JavaDoc

--- Properties/src/main/java/com/surefor/utils/properties/ProperitesExistException.java
+++ Properties/src/main/java/com/surefor/utils/properties/ProperitesExistException.java
@@ -1,7 +1,7 @@
 package com.surefor.utils.properties;
 
 /**
- * Created by chae on 4/10/2015.
+ * Duplicated properties
  */
 public class ProperitesExistException extends Exception {
     public ProperitesExistException(String message) {
--- logging/src/main/java/com/surefor/utils/logging/LogClassTransformer.java
+++ logging/src/main/java/com/surefor/utils/logging/LogClassTransformer.java
@@ -4,6 +4,11 @@ import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
+/**
+ * Injects commands before/after method.
+ * @author Ethan Cha
+ *
+ */
 public class LogClassTransformer implements ClassFileTransformer {
 
 	public byte[] transform(ClassLoader loader, String className,
